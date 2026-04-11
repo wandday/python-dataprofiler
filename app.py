71b33743f12f252b4a82a50cdd09f46f766667d5
Olawande Fatodu <wande@Olawandes-MacBook-Pro.local>
2026-04-11 01:46:35 +0100

    Switch to pandas-profiling for better Streamlit Cloud compatibility

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import pandas as pd
-from ydata_profiling import ProfileReport
+from pandas_profiling import ProfileReport
 import streamlit.components.v1 as components
 import sys
 import os
